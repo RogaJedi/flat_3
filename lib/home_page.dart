8da308e06d52ca055839a55ec3d95f431d9562ac
RogaJedi <7654595@gmail.com>
2024-09-28 12:11:19 +0300

    updated file structure

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'board_game_card.dart';
 import 'board_game_detail_page.dart';
 import 'notes.dart';
+import 'add_note_screen.dart';
 
 
 
@@ -27,17 +28,12 @@ final Sample3 = Note(
 );
 
 
-
-
-
-
 class HomePage extends StatefulWidget {
   @override
   _NotesListScreenState createState() => _NotesListScreenState();
 }
 
 class _NotesListScreenState extends State<HomePage> {
-  final TextEditingController _noteController = TextEditingController();
   List<Note> notes = [Sample1, Sample2, Sample3];
 
   void _navigateToAddNoteScreen(BuildContext context) async {
@@ -51,6 +47,12 @@ class _NotesListScreenState extends State<HomePage> {
         notes.add(result);
       });
     }
+  }
+
+  void addNewNote(Note newNote) {
+    setState(() {
+      notes.add(newNote);
+    });
   }
 
   void _removeNote(int index) async {
@@ -88,11 +90,6 @@ class _NotesListScreenState extends State<HomePage> {
     ) ?? false;
   }
 
-  void addNewNote(Note newNote) {
-    setState(() {
-      notes.add(newNote);
-    });
-  }
 
   Widget build(BuildContext context) {
 
@@ -140,78 +137,4 @@ class _NotesListScreenState extends State<HomePage> {
   }
 }
 
-class AddNoteScreen extends StatefulWidget {
-  @override
-  _AddNoteScreenState createState() => _AddNoteScreenState();
-  final Function(Note) onNoteAdded;
-  AddNoteScreen({required this.onNoteAdded});
-}
 
-class _AddNoteScreenState extends State<AddNoteScreen> {
-
-  final TextEditingController _noteControllerTitle = TextEditingController();
-  final TextEditingController _noteControllerShortInfo = TextEditingController();
-  final TextEditingController _noteControllerImageURL = TextEditingController();
-  final TextEditingController _noteControllerFullInfo = TextEditingController();
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Добавьте новую позицию'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            TextField(
-              scrollPadding: EdgeInsets.only(bottom: 40),
-              controller: _noteControllerTitle,
-              decoration: InputDecoration(
-                labelText: 'Название',
-              ),
-              maxLines: 5,
-            ),
-            TextField(
-              scrollPadding: EdgeInsets.only(bottom: 40),
-              controller: _noteControllerImageURL,
-              decoration: InputDecoration(
-                labelText: 'Изображение в формате URL',
-              ),
-              maxLines: 5,
-            ),
-            TextField(
-              scrollPadding: EdgeInsets.only(bottom: 40),
-              controller: _noteControllerShortInfo,
-              decoration: InputDecoration(
-                labelText: 'Краткое описание',
-              ),
-              maxLines: 5,
-            ),
-            TextField(
-              scrollPadding: EdgeInsets.only(bottom: 40),
-              controller: _noteControllerFullInfo,
-              decoration: InputDecoration(
-                labelText: 'Полное описание',
-              ),
-              maxLines: 5,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_noteControllerTitle.text.isNotEmpty && _noteControllerImageURL.text.isNotEmpty && _noteControllerShortInfo.text.isNotEmpty && _noteControllerFullInfo.text.isNotEmpty) {
-                  Note newNote = Note(name: _noteControllerTitle.text, text: _noteControllerShortInfo.text, imageUrl:_noteControllerImageURL.text, fullinfo: _noteControllerFullInfo.text);
-                  widget.onNoteAdded(newNote);
-                  Navigator.pop(context);
-                }
-              },
-              child: Text('Сохранить'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
