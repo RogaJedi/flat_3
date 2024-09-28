fea0a8cdcd9f9e52a4f826f5f4ceb35ea034584c
RogaJedi <7654595@gmail.com>
2024-09-28 11:51:52 +0300

    fixed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,28 +5,28 @@ import 'notes.dart';
 
 
 
-Note Sample1 = Note(
+final Note Sample1 = Note(
   name: 'Каркассон',
   text: 'Настольная игра про строительство города.',
   imageUrl: 'https://hobbyworld.cdnvideo.ru/image/cache/hobbyworld/data/HobbyWorld/Karkasson/1000/Karkasson00-1200x800.jpg',
   fullinfo: 'Настольная игра \"Каркассон\" пользуется славой одной из лучших настольных игр мира. В чём превосходство \"Каркассона\"? В простых правилах, в многообразии партий, в их небольшой продолжительности, в отсутствии открытой конфронтации между игроками – это классический образец игры \"в германском стиле\".'
 );
 
-Note Sample2 = Note(
+final Note Sample2 = Note(
     name: 'Unmatched',
     text: 'Настольная игра про сражения величайших легенд.',
     imageUrl: 'https://hobbygames.ru/image/data/Gaga/Unmatched_Bitva_Legend/UnMatched.jpg',
     fullinfo: 'Мифологические персонажи, герои мультфильмов и художественной литературы появляются на одном поле для сражения! "Unmatched. Битва Легенд" – игра с простыми правилами, воплощающая в реальность детские фантазии о невозможных встречах вымышленных героев из разных вселенных. Каждый том содержит в себе карты с новыми персонажами, которые можно мешать с колодами из других серий.'
 );
 
-Note Sample3 = Note(
+final Sample3 = Note(
     name: 'Подземелья и пёсики',
     text: 'Настольная игра про побег из подземелья.',
     imageUrl: 'https://hobbygames.ru/image/cache/hobbygames_beta/data/-new/hobby-world/podzemelja-i-pjosiki/podzemelja-i-pesiki-00-1024x1024-wm.jpg',
     fullinfo: 'Добро пожаловать на состязание по побегу из подземелья! Нашим смелым хорошим мальчикам и девочкам предстоит спуститься в очень глубокие помещения (мы не знаем, кто их вырыл) и дать отпор страшным монстрам! Но наши участники не простые пёсики, а самые настоящие приключенцы. Кто из них соберёт больше всего вкусняшек и первым покинет мрачное подземелье?'
 );
 
-List<Note> notes = [Sample1, Sample2, Sample3];
+
 
 
 
@@ -38,11 +38,12 @@ class HomePage extends StatefulWidget {
 
 class _NotesListScreenState extends State<HomePage> {
   final TextEditingController _noteController = TextEditingController();
+  List<Note> notes = [Sample1, Sample2, Sample3];
 
   void _navigateToAddNoteScreen(BuildContext context) async {
     final result = await Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => AddNoteScreen()),
+      MaterialPageRoute(builder: (context) => AddNoteScreen(onNoteAdded:  this.addNewNote)),
     );
 
     if (result != null && result.isNotEmpty) {
@@ -142,6 +143,8 @@ class _NotesListScreenState extends State<HomePage> {
 class AddNoteScreen extends StatefulWidget {
   @override
   _AddNoteScreenState createState() => _AddNoteScreenState();
+  final Function(Note) onNoteAdded;
+  AddNoteScreen({required this.onNoteAdded});
 }
 
 class _AddNoteScreenState extends State<AddNoteScreen> {
@@ -150,6 +153,8 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
   final TextEditingController _noteControllerShortInfo = TextEditingController();
   final TextEditingController _noteControllerImageURL = TextEditingController();
   final TextEditingController _noteControllerFullInfo = TextEditingController();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -198,10 +203,8 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
               onPressed: () {
                 if (_noteControllerTitle.text.isNotEmpty && _noteControllerImageURL.text.isNotEmpty && _noteControllerShortInfo.text.isNotEmpty && _noteControllerFullInfo.text.isNotEmpty) {
                   Note newNote = Note(name: _noteControllerTitle.text, text: _noteControllerShortInfo.text, imageUrl:_noteControllerImageURL.text, fullinfo: _noteControllerFullInfo.text);
-                  setState(() {
-                    notes.add(newNote);
-                  });
-                  Navigator.pop(context, newNote);
+                  widget.onNoteAdded(newNote);
+                  Navigator.pop(context);
                 }
               },
               child: Text('Сохранить'),
